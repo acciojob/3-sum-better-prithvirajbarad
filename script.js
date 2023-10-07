eb0f17bcd54a5056b755ac4482234058ff656862
prithvirajbarad <112259618+prithvirajbarad@users.noreply.github.com>
2023-10-07 11:09:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-function threeSum(arr, target) {
-// write your code here
-  
+  arr.sort((a, b) => a - b);
+
+  // Initialize variables to store the closest sum and its difference from the target.
+  let closestSum = Infinity;
+  let minDiff = Infinity;
+
+  // Iterate through the array with a fixed first element.
+  for (let i = 0; i < arr.length - 2; i++) {
+    // Two pointers approach for the remaining elements.
+    let left = i + 1;
+    let right = arr.length - 1;
+
+    while (left < right) {
+      const sum = arr[i] + arr[left] + arr[right];
+      const diff = Math.abs(sum - target);
+
+      // Check if the current sum is closer to the target.
+      if (diff < minDiff) {
+        minDiff = diff;
+        closestSum = sum;
+      }
+
+      if (sum < target) {
+        // Move the left pointer to the right to increase the sum.
+        left++;
+      } else {
+        // Move the right pointer to the left to decrease the sum.
+        right--;
+      }
+    }
+  }
+
+  return closestSum;
 }
 
 module.exports = threeSum;
